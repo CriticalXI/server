bced7804e82b32930854ac5e968c517de2cea8ed
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2026-09-03 01:27:59 -0400

    [yaml] Removes comments and adds nomad moogles

--- modules/phoenix/sql/nomad_moogle.sql
+++
@@ -1,8 +0,0 @@
--- ------------------------------------------------------------
--- Sets content tags for Nomad Moogles to be available on Phoenix
--- ------------------------------------------------------------
-
-UPDATE `npc_list` SET `content_tag` = 'TOAU' WHERE `npcid` = 17797251; -- Mhaura
-UPDATE `npc_list` SET `content_tag` = 'TOAU' WHERE `npcid` = 17797252; -- Mhaura
-UPDATE `npc_list` SET `content_tag` = 'TOAU' WHERE `npcid` = 17793129; -- Selbina
-UPDATE `npc_list` SET `content_tag` = 'TOAU' WHERE `npcid` = 17793130; -- Selbina
